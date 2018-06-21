c8cc2f5d1e3fe118609714a656bcc398b27d4a6f
Nils Kummert <n.kummert@gmail.com>
2018-06-21 18:08:44 +0200

    Added Front- and Backend functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from urllib.parse import urlsplit
+from urllib.parse import urlsplit, unquote
 from bs4 import BeautifulSoup
 import os
 import requests
@@ -7,31 +7,41 @@ import requests
 os.environ["FLASK_APP"] = "app.py"
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def edit():
     if request.method == 'GET':
         return render_template('edit_get.html')
-    else:
-        url = request.form.get('url')
+
+@app.route("/search", methods=['GET'])
+def searchURL():
+    if request.method == 'GET':
+        url = unquote(request.args.get('url'))
         print(url)
         return render_template('edit_post.html', url=url)
 
 @app.route("/proxy/<path:url>", methods=['GET'])
 def proxy(url):
-    print(request.args.get('url'))
-    r = requests.get(url)
-    c = r.content
-    soup = BeautifulSoup(c, "html.parser")
+    if not 'http://' in url and not 'https://' in url:
+        useable_url = 'http://' + url
+    else:
+        useable_url = url
+    print(useable_url)
+    try:
+        r = requests.get(useable_url)
+        c = r.content
+        soup = BeautifulSoup(c, "html.parser")
 
-    newtag_base = soup.new_tag('base')
-    base_url = "{0.scheme}://{0.netloc}/".format(urlsplit(url))
-    newtag_base.attrs['href'] = base_url
-    soup.head.insert(0, newtag_base)
+        newtag_base = soup.new_tag('base')
+        base_url = "{0.scheme}://{0.netloc}/".format(urlsplit(r.url))
+        newtag_base.attrs['href'] = base_url
+        soup.head.insert(0, newtag_base)
 
-    newtag_link = soup.new_tag('link')
-    newtag_link.attrs['rel'] = "stylesheet"
-    newtag_link.attrs['type'] = "text/css"
-    newtag_link.attrs['href'] = "/static/style.css"
-    soup.head.insert(0, newtag_link)
+        newtag_link = soup.new_tag('link')
+        newtag_link.attrs['rel'] = "stylesheet"
+        newtag_link.attrs['type'] = "text/css"
+        newtag_link.attrs['href'] = "/static/style.css"
+        soup.head.insert(0, newtag_link)
 
-    return soup.renderContents()
+        return soup.renderContents()
+    except:
+        return "Please provide a valid url"
--- static/script_edit.js
+++ static/script_edit.js
@@ -4,13 +4,20 @@ window.addEventListener('load', function(){
   var iframe = document.querySelector('iframe');
   var tagNodeList = [];
 
-  //add clickListener to Button to enter edit mode
-  var edit_button = document.querySelector('#trigger_edit');
-  edit_button.addEventListener('click', buttonHandler);
-
   //add clickListener to modal
   var pu = document.querySelector('.modal');
   var editable_content = document.querySelector('.textarea_edit');
+
+  //add clickListener to save and close
+  var save_button = document.querySelector('#save_changes');
+  var close_button = document.querySelector('.close');
+
+  //add clickListener to Button to enter edit mode
+  var edit_button = document.querySelector('#editButton');
+  var not_edit_button = document.querySelector('#noEditButton');
+  edit_button.addEventListener('click', addButtonHandler);
+  not_edit_button.addEventListener('click', removeButtonHandler);
+  saveAndClose();
 
   //add global variable for active Event
   var active_element;
@@ -27,7 +34,7 @@ window.addEventListener('load', function(){
     if (this.childElementCount > 0){
       var check = true;
       for (let i = 0; i < this.childElementCount; i++){
-        if (this.children[i].tagName !== "BR" || this.children[i].tagName !== "SPAN"){
+        if (this.children[i].tagName !== "BR" && this.children[i].tagName !== "SPAN" && this.children[i].tagName !== "B"){
           check = false;
         }
       }
@@ -44,29 +51,58 @@ window.addEventListener('load', function(){
     }
   }
 
-  function buttonHandler(){
+  function addButtonHandler(){
     for (let j = 0; j < editable_tags.length; j++){
-      tagNodeList.push(iframe.contentDocument.querySelectorAll(editable_tags[j]));
-      for (let i = 0; i< tagNodeList[j].length; i++){
-        tagNodeList[j][i].addEventListener('click', prepareEditTextContent);
+      let cacheTagList = iframe.contentDocument.querySelectorAll(editable_tags[j]);
+      if (cacheTagList.length > 0){
+        tagNodeList.push(cacheTagList);
+        for (let i = 0, index = tagNodeList.length-1; i< tagNodeList[index].length; i++){
+          tagNodeList[index][i].addEventListener('click', prepareEditTextContent);
+        }
+      }
+      else{
+      }
+    }
+    let divNodeList = [];
+    let cacheNodeList = [];
+    divNodeList.push(iframe.contentDocument.querySelectorAll('div'));
+    for (let i = 0; i < divNodeList; i++){
+      if(divNodeList[i].children === 0 && divNodeList[i].innerText.length > 0){
+        cacheNodeList.push(divNodeList[i]);
+      }
+      else{
+      }
+    }
+    if (cacheNodeList.length > 0){
+      tagNodeList.push(cacheNodeList);
+      for (let i = 0, index = tagNodeList.length-1; i < tagNodeList[index].length; i++){
+          tagNodeList[index][i].addEventListener('click', prepareEditTextContent);
+      }
+    }
+    console.log(tagNodeList);
+  }
+
+  function removeButtonHandler(){
+    for(let i=0; i < tagNodeList.length; i++){
+      for(let j=0; j < tagNodeList[i].length; j++){
+        tagNodeList[i][j].removeEventListener('click', prepareEditTextContent);
       }
     }
   }
 
   //add eventListener for save buttonHandler
-  var save_button = document.querySelector('#save_changes');
-  save_button.addEventListener('click', function(event){
-    event.preventDefault();
-    active_element.innerHTML = editable_content.value;
-    pu.style.display = 'none';
-    editable_content.innerHTML = "";
-  })
-
-  var close_button = document.querySelector('.close');
-  close_button.addEventListener('click', function(){
-    pu.style.display = "none";
-    editable_content.innerHTML = "";
-  })
+  function saveAndClose(){
+    save_button.addEventListener('click', function(event){
+      event.preventDefault();
+      active_element.innerHTML = editable_content.value;
+      pu.style.display = 'none';
+      editable_content.innerHTML = "";
+    });
+    close_button.addEventListener('click', function(){
+      pu.style.display = "none";
+      editable_content.innerHTML = "";
+    });
+  }
 
   var links = iframe.contentDocument.querySelectorAll('a');
   for (let i = 0; i < links.length; i++){
